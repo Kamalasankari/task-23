fc4c1ce4c822ef7942cf032e0b4d8c503b31c37e
Kamalasankari <142683264+Kamalasankari@users.noreply.github.com>
2023-12-02 13:38:58 +0530

    Update dragdrop.py

--- dragdrop.py
+++ dragdrop.py
@@ -5,34 +5,33 @@ from selenium.webdriver.common.by import By
 from time import sleep
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.action_chains import ActionChains
-
 class Dragdrop:
     def __init__(self):
         self.url = "https://jqueryui.com/droppable/"
         self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-
-
     def drag(self):
         try:
-            sleep(4)
+            sleep(2)
             self.driver.maximize_window()
-            sleep(4)
+            sleep(2)
             self.driver.get(self.url)
-            sleep(4)
+            sleep(2)
+            # to select the frame first
             frame = self.driver.find_element(by= By.XPATH,value= "//*[@id='content']/iframe")
             self.driver.switch_to.frame(frame)
+            # to locate draggable box
             source = self.driver.find_element(by=By.ID, value="draggable")
+            # to locate droppable box
             destination = self.driver.find_element(by=By.ID, value="droppable")
+            #Action chain to perform placing whitebox into the yellow box
             Actions = ActionChains(self.driver)
             Actions.drag_and_drop(source, destination).perform()
-
-            sleep(8)
+            sleep(3)
 
         except NoSuchElementException as selenium_error:
             print(selenium_error)
         finally:
             self.driver.close()
-
 
 drop= Dragdrop()
 drop.drag()
